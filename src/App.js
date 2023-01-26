2c6f3f6f010379faa500547fe3cb93cfccdd30f1
porterclev <porterclev@gmail.com>
2023-01-25 22:15:44 -0800

    comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import html2canvas from "html2canvas";
 function App() {
+  /* 
+    Takes screenshot of html body and prints 64 bit url in browser console
+  */
   const test = () => {
     html2canvas(document.body).then((canvas) => {
       const base64image = canvas.toDataURL("image/png");
@@ -10,6 +13,7 @@ function App() {
 
   return (
     <div className="App">
+      {/* Button triggers screenshot, output is sent to browser console */}
       <button onClick={test}>Button</button>
     </div>
   );
